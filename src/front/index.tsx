19fc593614aba7ed527cb5bdae3bc28da1ad3dd5
kbinani <kbinani.bt@gmail.com>
2021-12-27 06:58:09 +0900

    Skeleton react app

---
+++ src/front/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { MainComponent } from "./main";
+
+ReactDOM.render(<MainComponent />, document.getElementById("app"));
---
+++ src/front/main.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { FC } from "react";
+
+export const MainComponent: FC = () => {
+  return <div>Hello World!</div>;
+};
