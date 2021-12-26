62ccd8d7d778b7c3632c4c79cf7c27877fc32a9c
kbinani <kbinani.bt@gmail.com>
2021-12-27 07:10:57 +0900

    Input file

--- src/front/main.tsx
+++ src/front/main.tsx
@@ -1,6 +1,23 @@
 import * as React from "react";
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 
 export const MainComponent: FC = () => {
-  return <div>Hello World!</div>;
+  const [files, setFiles] = useState<FileList | null>(null);
+  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setFiles(ev.target.files);
+  };
+  const onStart = () => {
+    if (!files || files.length !== 1) {
+      console.error("no file selected, or one or more files selected");
+      return;
+    }
+  };
+  return (
+    <div>
+      <input type="file" onChange={onChange} />
+      <div className="button" onClick={onStart}>
+        Start
+      </div>
+    </div>
+  );
 };
