52dce42195f13e08c9b24595a6d98554224e2774
kbinani <kbinani.bt@gmail.com>
2021-12-30 22:22:19 +0900

    Use version 0.0.0 when empty

--- src/front/index.tsx
+++ src/front/index.tsx
@@ -6,10 +6,11 @@ import BugsnagPluginReact from "@bugsnag/plugin-react";
 import { kAppVersion } from "../share/version";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const appVersion = kAppVersion === "" ? "0.0.0" : kAppVersion;
   Bugsnag.start({
     apiKey: "0b173cbc2086cd69b797414d4cceafbb",
     plugins: [new BugsnagPluginReact()],
-    appVersion: kAppVersion,
+    appVersion,
   });
   const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
   ReactDOM.render(
