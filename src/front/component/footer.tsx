9e0ce3b7342dfca020539211f8debf0167026dab
kbinani <kbinani.bt@gmail.com>
2022-06-26 18:17:35 +0900

    Add store badges on the footer

--- src/front/component/footer.tsx
+++ src/front/component/footer.tsx
@@ -6,25 +6,54 @@ export const Footer: FC<{ onClickAbout: () => void }> = ({ onClickAbout }) => {
   return (
     <div className="footer">
       <div className="footerContent">
-        <div className={"footerRow"}>
-          <a
-            href={"https://github.com/kbinani/je2be-web"}
-            target={"je2be.app/github-link"}
-          >
-            <img
-              width={20}
-              height={20}
-              src={"image/GitHub-Mark-Light-64px.png"}
-            />
-          </a>
-        </div>
-        <div className="footerRow">
-          je2be-web {kAppVersion === "" ? "(local)" : kAppVersion}
-        </div>
         <div className="footerRow hFlex">
-          <div>Copyright © kbinani</div>・
+          <div>
+            <a
+              href={"https://github.com/kbinani/je2be-web"}
+              target={"je2be.app/github-link"}
+            >
+              <img
+                width={20}
+                height={20}
+                src={"image/GitHub-Mark-Light-64px.png"}
+              />
+            </a>
+          </div>
+          <div>&nbsp;</div>
+          <div>je2be-web {kAppVersion === "" ? "(local)" : kAppVersion}</div>
+          <div>・</div>
+          <div>Copyright © kbinani</div>
+          <div>・</div>
           <div onClick={onClickAbout} style={{ cursor: "pointer" }}>
             About je2be-web
+          </div>
+        </div>
+        <div className="hFlex">
+          <div style={{ margin: 10 }}>
+            <a
+              target={"_blank"}
+              href={"https://apps.apple.com/jp/app/je2be/id1617552608"}
+            >
+              <img
+                height={40}
+                alt={"Download je2be on the App Store"}
+                src={
+                  "image/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+                }
+              />
+            </a>
+          </div>
+          <div style={{ margin: 10 }}>
+            <a
+              target={"_blank"}
+              href={"https://www.microsoft.com/store/apps/9PC9MFX9QCXS"}
+            >
+              <img
+                height={39}
+                alt={"Get je2be from Microsoft Store"}
+                src={"image/English_get it from MS_864X312.svg"}
+              />
+            </a>
           </div>
         </div>
       </div>
