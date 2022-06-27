70a8a25372fb29dc46980363d9d4f40f0f830464
kbinani <kbinani.bt@gmail.com>
2022-06-27 10:03:13 +0900

    Change the number of digits of progress

--- src/front/component/progress.tsx
+++ src/front/component/progress.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import { FC } from "react";
 import { clamp } from "../../share/number";
 import { ConverterMetadata, Rational, Step } from "../../share/progress";
+import { sprintf } from "sprintf-js";
 
 export const Progress: FC<{
   progress: Rational;
   step: Step;
   meta: ConverterMetadata;
 }> = ({ progress, step, meta }) => {
-  const p =
-    clamp(Math.floor((progress.num / progress.den) * 1000), 0, 1000) / 10;
+  const p = clamp((progress.num / progress.den) * 100, 0, 100);
   const width = progress.num < 0 ? 100 : p;
   const unit = meta.displayUnit(step);
   const label = meta.stepDescription(step);
@@ -23,8 +23,11 @@ export const Progress: FC<{
       />
       <div className="progressLabel">
         {unit
-          ? `${label}: ${Math.max(0, progress.num)} ${unit}, ${p}% done`
-          : `${label}: ${p}% done`}
+          ? `${label}: ${Math.max(0, progress.num)} ${unit}, ${sprintf(
+              "%.1f",
+              p
+            )}% done`
+          : `${label}: ${sprintf("%.1f", p)}% done`}
       </div>
     </div>
   );
