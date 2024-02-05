ddcb84ef0e0a5eab09845e853414f4a95e5cb022
kbinani <kbinani.bt@gmail.com>
2024-02-05 11:14:48 +0900

    Update about dialog

--- src/front/component/about.tsx
+++ src/front/component/about.tsx
@@ -32,7 +32,7 @@ export const About: React.FC<{ onBack: () => void }> = ({ onBack }) => {
       <div className="about vFlex">
         <div className="aboutHeader">About je2be-web</div>
         <VSpace />
-        <div>Copyright (C) kbinani 2022-2023</div>
+        <div>Copyright (C) kbinani 2022-2024</div>
         <Link url={"https://github.com/kbinani/je2be-web"} />
         <VSpace />
         <div>
@@ -56,6 +56,17 @@ export const About: React.FC<{ onBack: () => void }> = ({ onBack }) => {
           &lt;
           <Link url={"https://www.gnu.org/licenses/"} />
           &gt;.
+        </div>
+        <VSpace />
+        <VSpace />
+        <div className="aboutHeader">Trademark notice</div>
+        <VSpace />
+        <div>
+          Xbox and Xbox 360 are trademarks of the Microsoft group of companies.
+          <br />
+          <br />
+          PS3 is a registered trademark of Sony Interactive Entertainment Inc.
+          <br />
         </div>
         <VSpace />
         <VSpace />
@@ -987,6 +998,114 @@ export const About: React.FC<{ onBack: () => void }> = ({ onBack }) => {
           CAUSED AND
           <br />
           ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+          TORT
+          <br />
+          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+          OF THIS
+          <br />
+          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        </DependsOn>
+        <VSpace />
+        <DependsOn name={"sparse"} url={"https://github.com/Arzaghi/sparse"}>
+          MIT License
+          <br />
+          <br />
+          Copyright (c) 2020 Hamid Reza Arzaghi
+          <br />
+          <br />
+          Permission is hereby granted, free of charge, to any person obtaining
+          a copy
+          <br />
+          of this software and associated documentation files (the "Software"),
+          to deal
+          <br />
+          in the Software without restriction, including without limitation the
+          rights
+          <br />
+          to use, copy, modify, merge, publish, distribute, sublicense, and/or
+          sell
+          <br />
+          copies of the Software, and to permit persons to whom the Software is
+          <br />
+          furnished to do so, subject to the following conditions:
+          <br />
+          <br />
+          The above copyright notice and this permission notice shall be
+          included in all
+          <br />
+          copies or substantial portions of the Software.
+          <br />
+          <br />
+          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+          EXPRESS OR
+          <br />
+          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+          MERCHANTABILITY,
+          <br />
+          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+          SHALL THE
+          <br />
+          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+          <br />
+          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+          ARISING FROM,
+          <br />
+          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+          IN THE
+          <br />
+          SOFTWARE.
+        </DependsOn>
+        <VSpace />
+        <DependsOn name={"lz4"} url={"https://github.com/lz4/lz4"}>
+          LZ4 Library
+          <br />
+          Copyright (c) 2011-2020, Yann Collet
+          <br />
+          All rights reserved.
+          <br />
+          <br />
+          Redistribution and use in source and binary forms, with or without
+          modification,
+          <br />
+          are permitted provided that the following conditions are met:
+          <br />
+          <br />
+          * Redistributions of source code must retain the above copyright
+          notice, this
+          <br />
+          list of conditions and the following disclaimer.
+          <br />
+          <br />
+          * Redistributions in binary form must reproduce the above copyright
+          notice, this
+          <br />
+          list of conditions and the following disclaimer in the documentation
+          and/or
+          <br />
+          other materials provided with the distribution.
+          <br />
+          <br />
+          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+          "AS IS" AND
+          <br />
+          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+          IMPLIED
+          <br />
+          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+          <br />
+          DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+          LIABLE FOR
+          <br />
+          ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+          DAMAGES
+          <br />
+          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+          SERVICES;
+          <br />
+          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+          CAUSED AND ON
+          <br />
+          ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
           TORT
           <br />
           (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
